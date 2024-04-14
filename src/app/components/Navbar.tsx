03c0abdd7e835e59c027c573460f25d650cf8904
Sayak-Mallick <sayakmallickkv@gmail.com>
2024-04-14 06:03:46 +0530

    [UPDATE] updated dependencies and added Navbar component

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+"use client"
+import Link from 'next/link'
+import { BiUserCircle, BiSearch } from 'react-icons/bi'
+import { RiArrowDropDownFill } from 'react-icons/ri'
+import Image from 'next/image'
+import LocationPopup from '@/popups/location/LocationPopup'
+
+
+export const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
