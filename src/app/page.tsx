ff05b2fcab4dd040fb2f80eec551e013a57e4e4c
sayakmallick4875 <sayak@solivatech.com>
2024-05-21 18:10:06 +0530

    chore: Update npm dependencies and add Swiper library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+"use client";
+
+import React from "react";
+import HomeSlider from "./components/HomeSlider";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        Home
+      <HomeSlider />
     </main>
   );
 }
