b3f11dd085ce095ac3a67249b94b246a7ca1e26f
sayakmallick4875 <sayak@solivatech.com>
2024-05-21 18:48:13 +0530

    chore: Add lazy loading for images in HomeSlider component

--- src/app/components/HomeSlider.tsx
+++ src/app/components/HomeSlider.tsx
@@ -1,22 +1,41 @@
 import React, { useRef, useState } from 'react';
+import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
 
 const HomeSlider = () => {
+	const [banners, setBanners] = useState([
+		{
+			imgUrl: 'https://assets-in.bmscdn.com/promotions/cms/creatives/1693561351496_motogpsepdesktop.jpg',
+		},
+		{
+			imgUrl: 'https://assets-in.bmscdn.com/promotions/cms/creatives/1693472198837_iccdesktop.jpg',
+		},
+	]);
+
+	const width = window.innerWidth;
+	const height = window.innerHeight;
+
 	return (
 		<div className='w-full'>
 			<Swiper rewind={true} navigation={true} modules={[Navigation]} className='mySwiper'>
-				<SwiperSlide>Slide 1</SwiperSlide>
-				<SwiperSlide>Slide 2</SwiperSlide>
-				<SwiperSlide>Slide 3</SwiperSlide>
-				<SwiperSlide>Slide 4</SwiperSlide>
-				<SwiperSlide>Slide 5</SwiperSlide>
-				<SwiperSlide>Slide 6</SwiperSlide>
-				<SwiperSlide>Slide 7</SwiperSlide>
-				<SwiperSlide>Slide 8</SwiperSlide>
-				<SwiperSlide>Slide 9</SwiperSlide>
+				{banners.map((banner, index) => {
+					return (
+						<SwiperSlide key={index}>
+							<Image
+								src={banner.imgUrl}
+								alt=''
+								width={width}
+								height={height / 2}
+								style={{
+									objectFit: 'cover',
+								}}
+							/>
+						</SwiperSlide>
+					);
+				})}
 			</Swiper>
 		</div>
 	);
