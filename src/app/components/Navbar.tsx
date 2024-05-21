b15137d41117dbf1b9c1b45e7e6a0316ecbbc1ca
sayakmallick4875 <sayak@solivatech.com>
2024-05-21 18:29:22 +0530

    chore: Add sticky positioning to Navbar component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,19 +5,23 @@ import Image from 'next/image';
 
 export const Navbar = () => {
   return (
-    <nav className="bg-[#F6F5F2] text-black flex justify-between items-center p-4">
+    <nav className="bg-[#F6F5F2] sticky top-0 text-black flex justify-between items-center p-4">
       <div className="flex items-center left">
         <Link href="/">
-        <Image
-          src="/logo.png"
-          alt="Logo"
-          width={100}
-          height={100}
-          className="h-8 mr-2"
-        />
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            width={100}
+            height={100}
+            className="h-8 mr-2"
+          />
         </Link>
         <div className="flex items-center bg-white rounded-md p-2 ml-2 w-80">
-          <input type="text" placeholder="Search For a Movie" className="outline-none ml-2" />
+          <input
+            type="text"
+            placeholder="Search For a Movie"
+            className="outline-none ml-2"
+          />
           <BiSearch className="text-yellow-200 text-lg mx-auto" />
         </div>
       </div>
