659554412363bda3197b0eb23aae4cb6ce3b15f3
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-02-21 07:55:09 +0530

    Refactor Repo

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['in.bmscdn.com','assets-in.bmscdn.com'], // Add the hostname(s) here
-    },
-};
-
-export default nextConfig;
